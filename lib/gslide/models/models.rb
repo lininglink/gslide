2e5d2f2b6baada7fdd6a4fc490bedcb5fd81fce7
Kang-Kyu Lee <kangkyu1111@gmail.com>
2025-03-30 18:30:21 -0700

    Add Google Sheets (for charts)

--- lib/gslide/models/models.rb
+++ lib/gslide/models/models.rb
@@ -4,6 +4,7 @@ require "gslide/models/editor"
 module Gslide
   module Models
     GOOGLE_SLIDES = "https://slides.googleapis.com/v1/presentations"
+    GOOGLE_SHEETS = "https://sheets.googleapis.com/v4/spreadsheets"
   end
 
   include Models
