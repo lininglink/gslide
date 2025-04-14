49485114bfa7d6ecb1a25718304ce74c7bece54c
Kang-Kyu Lee <kangkyu1111@gmail.com>
2025-04-14 10:46:24 -0700

    Add test for that

--- lib/gslide/models/presentation.rb
+++ lib/gslide/models/presentation.rb
@@ -7,13 +7,13 @@ module Gslide
 
       attr_reader :id
 
-      PRESENTATION_PATTERN = %r[/presentation/d/([a-zA-Z0-9-_]+)?]
+      PRESENTATION_PATTERN = %r[/presentation/d/([a-zA-Z0-9\-_]+)?]
 
       def initialize(id_or_url, auth: nil)
         @id = if url_id = id_or_url.match(PRESENTATION_PATTERN)
           url_id[1]
         else
-          id
+          id_or_url
         end
         @auth = auth
       end
--- test/test_gslide.rb
+++ test/test_gslide.rb
@@ -7,7 +7,17 @@ class TestGslide < Minitest::Test
     refute_nil ::Gslide::VERSION
   end
 
-  # def test_it_does_something_useful
-  #   assert false
-  # end
+  def test_initialized_presentation_with_url
+    url = "https://docs.google.com/presentation/d/1W11pzmSEH7EoZXiMITa-cOM1y9Ym6Yby9pj_2l5NPm8/edit?usp=sharing"
+
+    presentation = Gslide::Presentation.new(url, auth: nil)
+    assert_equal "1W11pzmSEH7EoZXiMITa-cOM1y9Ym6Yby9pj_2l5NPm8", presentation.id
+  end
+
+  def test_initialized_with_presentation_id
+    presentation_id = "1W11pzmSEH7EoZXiMITa-cOM1y9Ym6Yby9pj_2l5NPm8"
+
+    presentation = Gslide::Presentation.new(presentation_id, auth: nil)
+    assert_equal "1W11pzmSEH7EoZXiMITa-cOM1y9Ym6Yby9pj_2l5NPm8", presentation.id
+  end
 end
