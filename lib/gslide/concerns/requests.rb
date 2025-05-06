bbbe216272e4a630cf117147067f34d86ccb0616
Kang-Kyu Lee <kangkyu1111@gmail.com>
2025-05-06 12:00:52 -0700

    Refactor: response body; two new errors

--- lib/gslide/concerns/requests.rb
+++ lib/gslide/concerns/requests.rb
@@ -7,8 +7,20 @@ module Gslide
         req = Net::HTTP::Get.new(uri.to_s)
         req['Authorization'] = "Bearer #{auth_token}"
 
-        Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+        res = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
           http.request(req)
+        end
+        response_body = JSON(res.body)
+
+        case res
+        when Net::HTTPSuccess
+          response_body
+        when Net::HTTPUnauthorized
+          msg = response_body["error"] ? response_body["error"]["message"] : "Unauthorized"
+          raise Gslide::UnauthorizedError, msg
+        else
+          msg = response_body["error"] ? response_body["error"]["message"] : "HTTPError"
+          raise Gslide::HTTPError, msg
         end
       end
 
@@ -19,8 +31,28 @@ module Gslide
 
         req.body = body
 
-        Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+        res = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
           http.request(req)
+        end
+
+        response_body = JSON(res.body)
+
+        case res
+        when Net::HTTPSuccess
+          response_body
+        when Net::HTTPUnauthorized
+          msg = response_body["error"] ? response_body["error"]["message"] : "Unauthorized"
+          raise Gslide::UnauthorizedError, msg
+        when Net::HTTPTooManyRequests
+          # "Quota exceeded for quota metric 'Write requests' and limit 'Write requests per minute per user' of service 'slides.googleapis.com' for consumer 'project_number:012345678901'."
+          if response_body["error"] && response_body["error"]["message"] =~ /Quota exceeded/
+            raise Gslide::QuotaExceededError, msg
+          else
+            raise Gslide::HTTPError, msg || "TooManyRequests"
+          end
+        else
+          msg = response_body["error"] ? response_body["error"]["message"] : "HTTPError"
+          raise Gslide::HTTPError, msg
         end
       end
     end
